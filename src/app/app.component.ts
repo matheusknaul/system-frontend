4c060cdffa3a8887782345dd519f67c8afc72abd
matheusknaul <contatomatheush0909@gmail.com>
2025-06-12 22:33:03 -0300

    refactor: all portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component";
-import { HomeComponent } from './components/home/home.component';
-import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'system-frontend';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,3 @@
 import { Routes } from '@angular/router';
-import { ContactComponent } from './components/contact/contact.component';
-import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [
-    {
-        path:"",
-        component: HomeComponent
-    }
-    ,{
-        path:"contact",
-        component: ContactComponent
-    }
-];
+export const routes: Routes = [];
